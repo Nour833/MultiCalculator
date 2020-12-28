f6d7826cf5f24fe23d2b2ca4630cca5a060b6ef8
Nour833 <68251939+Nour833@users.noreply.github.com>
2020-12-28 20:03:40 +0100

    Update equation.py

--- equation.py
+++ equation.py
@@ -4,8 +4,6 @@ def equation():
     print(colored("----------------------------------------------------------------", "magenta"))
     print(colored("1 ", "green"), colored(".", "red"), colored("1 dgree", "yellow"))
     print(colored("2 ", "green"), colored(".", "red"), colored("2 degree", "yellow"))
-    print(colored("3 ", "green"), colored(".", "red"), colored("3 degree", "yellow"))
-    print(colored("4 ", "green"), colored(".", "red"), colored("4 degree", "yellow"))
     print(colored("98", "green"), colored(".", "red"), colored("return to home menu", "yellow"))
     print(colored("99", "green"), colored(".", "red"), colored("exit", "yellow"))
     try :
@@ -14,28 +12,51 @@ def equation():
     except :
         print(colored("please select the right number", "red"))
         equation()
-    if aq == 1:
-        x=input(colored("Enter the equation :","yellow"))
-        x=x.lower()
-        print(x)
-        print(string.punctuation)
-        lst=x.split(string.punctuation)
-        print(lst)
-        for i in lst:
-            if "x" in i:
-                b = i.split("x")
-                b=b[0]
-                bx=b+"x"
-                xb="x"+b
+    try :
+        if aq == 1:
+            print(colored("Exemple : ax+b ", "red"))
+            b = int(input(colored("Enter b : ", "green")))
+            a = int(input(colored("Enter a : ", "green")))
+            x = -b/a
+            if type(x) == float:
+                print(colored("x = " + str(x), "magenta"),colored("or (another form) x = " + str(-b) + "/ "+str(a), "magenta"))
             else:
-                continue
-        if bx in x:
-            others = x.split(bx)
-        elif xb in x:
-            others = x.split(xb)
-        print(others)
-    elif aq == 98:
-        eq=1
-        return eq
-    elif aq == 99:
-        exit()
+                print(colored("x = "+str(x),"magenta"))
+        elif aq == 2:
+            a = int(input(colored("Enter a : ","green")))
+            b = int(input(colored("Enter b : ", "green")))
+            c = int(input(colored("Enter c : ","green")))
+            Delta = b ** 2 - 4 * a * c
+            rdelta = Delta**(1/2)
+            if type(rdelta)==float:
+                rDelta= "√"+str(Delta)
+            else:
+                rDelta = rdelta
+            if Delta == 0:
+                x = -b / (2 * a)
+                if type(x)==float:
+                    print(colored("x = "+str(x),"cyan")," or ",colored("x = "+str(-b)+"/ "+str(2*a),"cyan"))
+            elif Delta < 0:
+                print("X hasn't solution")
+            else:
+               x1 = (-b + rdelta) / (2 * a)
+               x2 = (-b - rdelta) / (2 * a)
+               if type(x1) == float:
+                  x1 = str(x1)+" or/or "+"( "+str(-b)+"+"+str(rDelta)+" ) "+"/ "+str(a*2)
+               else:
+                    x1 = x1
+               if type(x2) == float:
+                    x2 = str(x2)+" or/or "+"( "+str(-b)+"-"+str(rDelta)+" ) "+"/ "+str(a*2)
+               else:
+                    x2=x2
+               print(colored("x1 = "+x1,"cyan"),"and",colored("x2 = "+x2,"cyan"))
+
+
+        elif aq == 98:
+            eq=1
+            return eq
+        elif aq == 99:
+            exit()
+    except:
+        print(colored("please select the right number", "red"))
+        equation()
