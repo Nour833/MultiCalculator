c516d8dc7b25afe680cbc4dbd0c7bae8c6fc9b6a
Nour833 <68251939+Nour833@users.noreply.github.com>
2021-01-10 00:32:34 +0100

    Update tallf.py

--- tallf.py
+++ tallf.py
@@ -1,18 +1,17 @@
-
 def tallf():
-         h=int(input("""
+    h = int(input("""
          1.Girl
          2.Boy
          ==>"""))
-         if   h==1 :
-           x=float(input("Enter your father height please(cm) : "))
-           y=float(input("Enter your mother height please (cm): "))
-           s=((x-13)+y)/2
-           print ("Your futur heigth will be : "+str(s)+"cm")
-         elif   h==2 :
-           x=float(input("Enter your father height please(cm) : "))
-           y=float(input("Enter your mother height please (cm): "))
-           s=((x+13)+y)/2
-           print ("Your futur heigth will be : "+str(s)+"cm")
-         else :
-             print("please select the right number")
+    if h == 1:
+        x = float(input("Enter your father height please(cm) : "))
+        y = float(input("Enter your mother height please (cm): "))
+        s = ((x - 13) + y) / 2
+        print("Your future height will be : " + str(s) + "cm")
+    elif h == 2:
+        x = float(input("Enter your father height please(cm) : "))
+        y = float(input("Enter your mother height please (cm): "))
+        s = ((x + 13) + y) / 2
+        print("Your future height will be : " + str(s) + "cm")
+    else:
+        print("please select the right number")
