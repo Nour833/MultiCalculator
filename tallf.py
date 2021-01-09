e6d572d51e75f5070de93a353ef5f52c11daa8fd
Nour833 <68251939+Nour833@users.noreply.github.com>
2021-01-10 00:35:58 +0100

    Update tallf.py

--- tallf.py
+++ tallf.py
@@ -1,17 +1,21 @@
+from termcolor import colored
+
 def tallf():
     h = int(input("""
          1.Girl
          2.Boy
          ==>"""))
-    if h == 1:
-        x = float(input("Enter your father height please(cm) : "))
-        y = float(input("Enter your mother height please (cm): "))
-        s = ((x - 13) + y) / 2
-        print("Your future height will be : " + str(s) + "cm")
-    elif h == 2:
-        x = float(input("Enter your father height please(cm) : "))
-        y = float(input("Enter your mother height please (cm): "))
-        s = ((x + 13) + y) / 2
-        print("Your future height will be : " + str(s) + "cm")
-    else:
-        print("please select the right number")
+    try:
+        if h == 1:
+            x = float(input("Enter your father height please(cm) : "))
+            y = float(input("Enter your mother height please (cm): "))
+            s = ((x - 13) + y) / 2
+            print("Your future height will be : " + str(s) + "cm")
+        elif h == 2:
+            x = float(input("Enter your father height please(cm) : "))
+            y = float(input("Enter your mother height please (cm): "))
+            s = ((x + 13) + y) / 2
+            print("Your future height will be : " + str(s) + "cm")
+    except:
+        print(colored("please select the right number", "red"))
+        tallf()
